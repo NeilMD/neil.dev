f07392a805f7164c3f726f4829e63d4a2f04ab04
neilmd <neil.capistrano2007@gmail.com>
2025-02-25 02:10:23 -0700

    Added Project Links

---
+++ script.js
@@ -0,0 +1,10 @@
+const bc_link = "https://neilmd.github.io/CPRG-309_Final/";
+const ns_link = "https://neilmd.github.io/CPRG_310_NEW_FINAL/";
+
+document.getElementById("bc_card").addEventListener("click", () => {
+  window.open(bc_link, "_blank");
+});
+
+document.getElementById("ns_card").addEventListener("click", () => {
+  window.open(ns_link, "_blank");
+});
