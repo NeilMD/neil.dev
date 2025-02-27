b564c6c0733b49a033c285201a2863da1ec6175c
neilmd <neil.capistrano2007@gmail.com>
2025-02-26 17:19:10 -0700

    Final link

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,7 @@ export default function RootLayout({ children }) {
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Neil Capistrano | Portfolio</title>
-        <link rel="icon" href="/img/neil_thinking.webp" sizes="any" />
+        <link rel="icon" href="/neil.dev/img/neil_thinking.webp" sizes="any" />
       </head>
       <body className="bg-n-bg">{children}</body>
     </html>
